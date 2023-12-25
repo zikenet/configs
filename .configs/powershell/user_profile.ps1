9aa7ff95af44c92c44ffa7406f479a84d2c5c44c
zikenet <zikenet116@gmail.com>
2023-12-25 00:58:34 -0800

    tig and less removed from git

--- .configs/powershell/user_profile.ps1
+++ .configs/powershell/user_profile.ps1
@@ -4,8 +4,6 @@ Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
 # PSReadLine
 
