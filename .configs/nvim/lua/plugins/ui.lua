270b3b0704667674713a16d76f3f2ebe1c7e7186
zikenet <zikenet116@gmail.com>
2024-03-02 06:03:37 -0800

    incline added

--- .configs/nvim/lua/plugins/ui.lua
+++ .configs/nvim/lua/plugins/ui.lua
@@ -6,6 +6,30 @@ return {
       opts.background_colour = "#89b4fa"
     end,
   },
+  --incline
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local devicons = require("nvim-web-devicons")
+      require("incline").setup({
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          local modified = vim.bo[props.buf].modified
+          if modified then
+            filename = "[+]" .. filename
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          return { { ft_icon, guifg = ft_color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
   --nvim-noice
   {
     "folke/noice.nvim",
