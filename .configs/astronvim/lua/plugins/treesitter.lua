e550a6b26a40f51eb6eaf34789efa43653ee4926
zikenet <zikenet116@gmail.com>
2024-04-17 08:36:21 +0300

    treesitter languages added

--- .configs/astronvim/lua/plugins/treesitter.lua
+++ .configs/astronvim/lua/plugins/treesitter.lua
@@ -8,8 +8,27 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "json",
+      "javascript",
+      "typescript",
+      "python",
+      "tsx",
+      "yaml",
+      "html",
+      "css",
+      "prisma",
+      "markdown",
+      "markdown_inline",
+      "svelte",
+      "graphql",
+      "bash",
       "lua",
       "vim",
+      "dockerfile",
+      "gitignore",
+      "query",
+      "vimdoc",
+      "c",
       -- add more arguments for adding more treesitter parsers
     })
   end,
