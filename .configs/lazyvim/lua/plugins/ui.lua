560024a62cdd37df230d2fc2838a2802fc434242
zik <zik@ziks-MacBook-Pro.local>
2024-12-19 13:03:16 +0300

    doc border setup

--- .configs/lazyvim/lua/plugins/ui.lua
+++ .configs/lazyvim/lua/plugins/ui.lua
@@ -29,6 +29,14 @@ return {
       }
     end,
   },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
   --incline
   -- {
   --   "b0o/incline.nvim",
