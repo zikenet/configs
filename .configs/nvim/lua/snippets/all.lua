aba34ee9de2051c5d61bde79bac6110a05d8e09f
zak <zik@zaks-MacBook-Pro.local>
2025-07-04 05:48:03 -0700

    snippets fix

--- .configs/nvim/lua/snippets/all.lua
+++ .configs/nvim/lua/snippets/all.lua
@@ -1,12 +1,14 @@
 local luasnip = require("luasnip")
+
 local s = luasnip.snippet
 local t = luasnip.text_node
--- local i = luasnip.insert_node
+local i = luasnip.insert_node
 
 local consoleSnippet = s("cl", {
-  t("console.log()"),
+  t("console.log("),
+  i(0), -- Cursor will be here after expansion
+  t(")"),
 })
-
 for _, ft in ipairs({ "javascript", "typescript", "typescriptreact" }) do
   luasnip.add_snippets(ft, { consoleSnippet })
 end
