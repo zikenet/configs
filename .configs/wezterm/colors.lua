1b8587ef5c31014e43f00466fda83ab004db4d89
zikenet <zikenet116@gmail.com>
2025-12-26 04:18:45 -0800

    setup fixes

--- .configs/wezterm/colors.lua
+++ .configs/wezterm/colors.lua
@@ -2,6 +2,7 @@ local module = {}
 
 module.primary = "#FFFFFF"
 module.secondary = "#FFFFFF"
+module.dim = "#9c9c9c"
 module.alternative = "#EBFFE3"
 module.disabled = "#9C9C9C"
 module.primaryBackground = "#21252b"
--- .configs/wezterm/status-bar.lua
+++ .configs/wezterm/status-bar.lua
@@ -1,9 +1,8 @@
 local wezterm = require("wezterm")
 local colors = require("colors")
-local utils = require("utils")
 local module = {}
 
-function module.apply_to_config(config)
+function module.apply_to_config(_)
 	wezterm.on("update-status", function(window, pane)
 		local stat = window:active_workspace():upper()
 
@@ -18,26 +17,30 @@ function module.apply_to_config(config)
 		-- 	return path:match("([^/\\]+)[/\\]?$")
 		-- end
 		--
-		-- local directoryName = pane:get_current_working_dir()
-		-- wezterm.log_info(directoryName.file_path)
-		-- local cwd = directoryName.path:gsub("^/", "")
-		-- if cwd then
-		-- 	directoryName = winBaseName(cwd) --> URL object introduced in 20240127-113634-bbcac864 (type(directoryName) == "userdata")
-		-- else
-		-- 	directoryName = ""
-		-- end
+		local directoryName = pane:get_current_working_dir()
+		wezterm.log_info(directoryName.file_path)
+		local cwd = directoryName.path:gsub("^/", "")
+		if cwd then
+			directoryName = basename(cwd) --> URL object introduced in 20240127-113634-bbcac864 (type(directoryName) == "userdata")
+		else
+			directoryName = ""
+		end
 		local processName = pane:get_foreground_process_name()
 		processName = processName and basename(processName) or ""
 
 		window:set_left_status(wezterm.format({
 			{ Foreground = { Color = colors.dim } },
 			{ Text = " " },
-			{ Text = " " .. "[" .. stat .. "]" },
+			{ Text = " " .. "[" .. stat .. "]" .. "  " },
 		}))
 
 		window:set_right_status(wezterm.format({
 			{ Foreground = { Color = colors.dim } },
-			{ Text = wezterm.nerdfonts.fa_code .. " " .. processName .. " " },
+			{ Text = "  " .. directoryName },
+			{ Text = " | " },
+			{ Foreground = { Color = colors.dim } },
+			{ Text = wezterm.nerdfonts.fa_code .. "  " .. processName .. " " },
+			{ Text = " " },
 		}))
 	end)
 end
