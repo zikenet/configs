57d103ce413f640a9adf779fed6f2cfa51d88ffc
zikenet <zikenet116@gmail.com>
2026-01-02 06:22:08 -0800

    fish path fix

--- .configs/fish/functions/fzf_change_directory.fish
+++ .configs/fish/functions/fzf_change_directory.fish
@@ -1,18 +1,18 @@
 function _fzf_change_directory
-  fzf | perl -pe 's/([ ()])/\\\\$1/g'|read foo
-  if [ $foo ]
-    builtin cd $foo
-    commandline -r ''
-    commandline -f repaint
-  else
-    commandline ''
-  end
+    fzf --style full --layout=reverse | perl -pe 's/([ ()])/\\\\$1/g' | read foo
+    if [ $foo ]
+        builtin cd $foo
+        commandline -r ''
+        commandline -f repaint
+    else
+        commandline ''
+    end
 end
 
 function fzf_change_directory
-  begin
-    echo $HOME/.config
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/Developments/*/* |grep -v \.git
-  end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+    begin
+        echo $HOME/.config
+        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
+        ls -ad $HOME/Development/Projects/* | grep -v \.git
+    end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
