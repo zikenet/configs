fccfb59bcb4208d742fdf64e34c0b20264aa1c97
zikenet <zikenet116@gmail.com>
2026-01-29 07:51:09 -0800

    sesh prompt updates

--- .configs/fish/functions/sesh_sessions.fish
+++ .configs/fish/functions/sesh_sessions.fish
@@ -5,15 +5,11 @@ function sesh_sessions
             --no-sort \
             --ansi \
             --border rounded \
-            --prompt '⚡  ' \
-            --header '  ^a all ^o tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+            --prompt '⇒  ' \
             --bind 'tab:down,btab:up' \
-            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
-            --bind 'ctrl-o:change-prompt(  )+reload(sesh list -t --icons)' \
-            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
-            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+            --bind 'ctrl-a:change-prompt(⇒  )+reload(sesh list --icons)' \
             --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~/Development/ ~/.config/)' \
-            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+reload(sesh list --icons)'
+            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)'
     )
 
     # If nothing selected, just return
