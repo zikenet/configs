3996b66bcd68b7904ce8785a33d5fddba152f840
zikenet <zikenet116@gmail.com>
2026-01-31 13:54:47 -0500

    fish cleanup

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -2,12 +2,7 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
-# theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
+fish_vi_key_bindings
 
 # aliases
 alias ls "ls -p -G"
@@ -36,5 +31,3 @@ set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
-
-mise activate fish | source
