fe465617debeaa20ca57042d8f6b74cd5fb2ea48
zikenet <zikenet116@gmail.com>
2026-06-26 12:45:47 +0300

    lualine edit

--- .configs/nvim/lua/plugins/ui.lua
+++ .configs/nvim/lua/plugins/ui.lua
@@ -151,7 +151,6 @@ return {
       },
     },
   },
-
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
@@ -168,7 +167,18 @@ return {
         theme = theme,
         globalstatus = vim.o.laststatus == 3,
         disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-        -- section_separators = { left = "", right = "" },
+      }
+      opts.sections = {
+        lualine_a = { { "mode", separator = { left = "", right = "" }, right_padding = 2 } },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { right = "" },
+            left_padding = 2,
+          },
+        },
       }
     end,
   },
